466d40c09e1326938df4c739ae38a66c64f037cf
Verix99 <verix99@seznam.cz>
2023-03-15 16:42:32 +0100

    15.3

--- Dluznicek_2/main.js
+++ Dluznicek_2/main.js
@@ -4,7 +4,6 @@ window.addEventListener("load", () => {
   const number = document.querySelector("#new-number-input");
   const list_el = document.querySelector("#tasks");
   const debt = document.querySelector("#new-debt-input");
-  let dluh = 0;
   let platby = 0;
   let count = 0;
   document.getElementById("new-task-submit").onclick = function () {
@@ -12,11 +11,88 @@ window.addEventListener("load", () => {
     document.getElementById("demo").innerHTML = count;
   }; //sčítání členů
 
+<<<<<<< Updated upstream
   document.getElementById("new-debt-submit").onclick = function () {
     platby += 1; //sčítání členů
+=======
+  //Funkce pro přidávání členů
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const task = input.value;
+    var num = number.value;
+
+    if (!task) {
+      alert("Zapište člena!");
+      count -= 1;
+      document.getElementById("demo").innerHTML = count;
+      return;
+    }
+
+    const task_el = document.createElement("div");
+    task_el.classList.add("task");
+
+    const task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+
+    task_el.appendChild(task_content_el);
+
+    const task_input_el = document.createElement("input");
+    task_input_el.classList.add("text");
+    task_input_el.setAttribute("id", "clen_jmeno");
+    task_input_el.type = "text";
+    task_input_el.value = task;
+    task_input_el.setAttribute("readonly", "readonly");
+
+    let task_number_el = document.createElement("input");
+    task_number_el.classList.add("number");
+    task_number_el.setAttribute("id", "clen_penize" + [count]);
+    task_number_el.type = "number";
+    task_number_el.value = num;
+    task_number_el.setAttribute("readonly", "readonly");
+
+    task_content_el.appendChild(task_input_el);
+    task_content_el.appendChild(task_number_el);
+
+    const task_action_el = document.createElement("div");
+    task_action_el.classList.add("actions");
+
+    const task_edit_el = document.createElement("input");
+    task_edit_el.setAttribute("id", "checkbox_clen" + [count]);
+    task_edit_el.classList.add("edit");
+    task_edit_el.type = "checkbox";
+
+    const task_delete_el = document.createElement("button");
+    task_delete_el.classList.add("delete");
+    task_delete_el.innerHTML = "Delete";
+
+    task_action_el.appendChild(task_edit_el);
+    task_action_el.appendChild(task_delete_el);
+
+    task_el.appendChild(task_action_el);
+
+    list_el.appendChild(task_el);
+
+    input.value = "";
+    number.value = "";
+
+    //mazání členů
+    task_delete_el.addEventListener("click", () => {
+      list_el.removeChild(task_el);
+      count -= 1;
+      document.getElementById("demo").innerHTML = count;
+    });
+  });
+  //Funkce která počítá transakce
+  document.getElementById("new-debt-submit").onclick = function () {
+    var dluh = 0;
+    platby += 1;
+>>>>>>> Stashed changes
     document.getElementById("platba").innerHTML = platby;
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     const debt_castka = debt.value;
+    debt_pocitani = parseInt(debt_castka);
+    dluh += debt_pocitani;
     function countChecked() {
       let count_1 = 0;
       checkboxes.forEach(function (checkbox) {
@@ -31,9 +107,29 @@ window.addEventListener("load", () => {
     let vysledna_castka_debt = debt_castka / cislo_checkboxu;
     console.log(`Počet zaškrtnutých checkboxů: ${countChecked()}`);
     console.log(vysledna_castka_debt);
+<<<<<<< Updated upstream
     dluh = dluh + debt_castka;
     document.getElementById("dluh").innerHTML = dluh;
     debt.value = "";
+=======
+    let a = 0;
+    let checking = document.querySelectorAll('input[type="checkbox"]').length;
+    document.getElementById("dluh").innerHTML = dluh;
+    for (let i = 0; i <= checking; i++) {
+      a++;
+      var user = document.getElementById("clen_penize" + [a]).value;
+      let bobik = parseInt(user);
+      let total = bobik + vysledna_castka_debt;
+      var check = document.getElementById("checkbox_clen" + [a]);
+      if (check.checked) {
+        document.getElementById("clen_penize" + [a]).value = total;
+        console.log(typeof debt_castka);
+        debt.value = "";
+      }
+    }
+
+    console.log(checking);
+>>>>>>> Stashed changes
   };
 
   form.addEventListener("submit", (e) => {
